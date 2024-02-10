9321e05638241b998783f5333d20ec40f5e4c04f
themehdiq <qazzih.mehdi97@gmail.com>
2024-02-10 22:01:50 +0100

    sections done without bottons animation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily:{
+        barlow: ['Barlow Condensed']
+      }
+    },
+
+  },
+  plugins: [],
+}
+
