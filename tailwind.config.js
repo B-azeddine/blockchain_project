025833c315fd2b6c16b32028c6a60f667fc79b04
Nassime <nassimeelkamari2002@gmail.com>
2024-02-10 11:55:30 +0100

    My first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        akrobat: ['Akrobat', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
